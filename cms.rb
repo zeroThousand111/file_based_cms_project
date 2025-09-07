0248c1fef420ba9e058f5a46519050fbf42f8a79
Graham Jarvis <zerothousand111@proton.me>
2025-09-07 12:01:08 +0100

    implemented assignment 3 lesson 8 RB175 - adding an index page

--- cms.rb
+++ cms.rb
@@ -2,6 +2,11 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "tilt/erubi"
 
+root = File.expand_path("..", __FILE__)
+
 get "/" do
-  "<p>Getting started.</p>"
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+  erb :index
 end
