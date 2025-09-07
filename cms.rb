1feb01ac84b80c489ef265a672527139bb90e705
Graham Jarvis <zerothousand111@proton.me>
2025-09-07 20:29:17 +0100

    implemented assignment 4 lesson 8 RB175 - viewing text files

--- cms.rb
+++ cms.rb
@@ -10,3 +10,9 @@ get "/" do
   end
   erb :index
 end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
+end
