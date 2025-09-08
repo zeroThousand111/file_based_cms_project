7542f4d3f633f70e83576e1fda072f0f3b834eec
Graham Jarvis <zerothousand111@proton.me>
2025-09-08 14:21:08 +0100

    implemented assignment 6 lesson 8 RB175 - handling requests for nonexistent documents

--- cms.rb
+++ cms.rb
@@ -2,17 +2,32 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "tilt/erubi"
 
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
 root = File.expand_path("..", __FILE__)
 
-get "/" do
+before do
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end
+end
+
+get "/" do
   erb :index
 end
 
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+  filename = params[:filename]
+  file_path = root + "/data/" + filename
+
+  if File.file?(file_path)
+    headers["Content-Type"] = "text/plain"
+    File.read(file_path)
+  else
+    session[:error] = "#{filename} does not exist."
+    redirect "/"
+  end
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -29,4 +29,19 @@ class CMSTest < Minitest::Test
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "Ruby 0.95 released"
   end
+
+  def test_document_not_found
+    get "/notafile.ext" # Attempt to access a nonexistent file
+  
+    assert_equal 302, last_response.status # Assert that the user was redirected
+  
+    get last_response["Location"] # Request the page that the user was redirected   to
+  
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist"
+  
+    get "/" # Reload the page
+    refute_includes last_response.body, "notafile.ext does not exist" # Assert   that our message has been removed
+  end
+
 end
