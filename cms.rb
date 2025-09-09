0d3207dcbbdece11b385cab002f4f5ba66ae9fa9
Graham Jarvis <zeroThousand111@proton.me>
2025-09-09 20:47:28 +0100

    implemented assignment 9 lesson 8 RB175 - isolating test execution

--- cms.rb
+++ cms.rb
@@ -8,6 +8,14 @@ configure do
   set :session_secret, SecureRandom.hex(32)
 end
 
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
 root = File.expand_path("..", __FILE__)
 
 def render_markdown(text)
@@ -27,14 +35,15 @@ def load_file_content(path)
 end
 
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
     File.basename(path)
   end
   erb :index
 end
 
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   if File.exist?(file_path)
     load_file_content(file_path)
@@ -45,7 +54,7 @@ get "/:filename" do
 end
 
 get "/:filename/edit" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   @filename = params[:filename]
   @content = File.read(file_path)
@@ -54,7 +63,7 @@ get "/:filename/edit" do
 end
 
 post "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   File.write(file_path, params[:content])
 
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -1,9 +1,12 @@
+# test/cms_test.rb
 ENV["RACK_ENV"] = "test"
+
+require "fileutils"
 
 require "minitest/autorun"
 require "rack/test"
 
-require_relative "../cms.rb"
+require_relative "../cms"
 
 class CMSTest < Minitest::Test
   include Rack::Test::Methods
@@ -11,18 +14,36 @@ class CMSTest < Minitest::Test
   def app
     Sinatra::Application
   end
-  
+
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
     get "/"
 
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
   end
 
   def test_viewing_text_document
+    create_document "history.txt", "Ruby 0.95 released"
+
     get "/history.txt"
 
     assert_equal 200, last_response.status
@@ -30,27 +51,30 @@ class CMSTest < Minitest::Test
     assert_includes last_response.body, "Ruby 0.95 released"
   end
 
-  def test_document_not_found
-    get "/notafile.ext" # Attempt to access a nonexistent file
-    assert_equal 302, last_response.status
-
-    get last_response["Location"] # Request the page that the user was redirected to
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "notafile.ext does not exist"
-  
-    get "/" # Reload the page
-    refute_includes last_response.body, "notafile.ext does not exist"
-  end
-
   def test_viewing_markdown_document
+    create_document "about.md", "# Ruby is..."
+
     get "/about.md"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
 
-  # test/cms_test.rb
+  def test_document_not_found
+    get "/notafile.ext"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist"
+  end
+
   def test_editing_document
+    create_document "changes.txt"
+
     get "/changes.txt/edit"
 
     assert_equal 200, last_response.status
@@ -71,5 +95,4 @@ class CMSTest < Minitest::Test
     assert_equal 200, last_response.status
     assert_includes last_response.body, "new content"
   end
-
 end
